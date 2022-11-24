9b52913f70623c73ba45b8690ee4477a6fd33fa1
naomina121 <naomina@me.com>
2022-11-25 02:48:32 +0900

    first commit
    初めてのコミット

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+module.exports = {
+  siteMetadata: {
+    title: "デリペイのサイト",
+    description: "デリペイの公式サイト",
+  },
+  plugins: [
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.CONTENTFUL_HOST
+      },
+    },
+  ],
+};
